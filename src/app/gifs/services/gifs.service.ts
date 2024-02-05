4f09e587b0b8f505667e419e73fb4225757a0b96
VN - Alvaro Hdz Valero <alvaro.hernandezv@viewnext.com>
2024-02-05 14:35:13 +0100

    S07 - Validaciones al servicio

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -5,17 +5,32 @@ export class GifsService {
 
   private _tagsHistory: string[] = [];
 
-
   constructor() { }
-
 
   get tagsHistory(): string[] {
     return [...this._tagsHistory];
   }
 
-  searchTag( tag: string ): void {
+  private organizeHistory(tag: string) {
+    //* convierte a minúsculas el texto introducido en el buscador
+    tag = tag.toLowerCase();
 
+    //* filtra el contenido en la lista para que solo pase el contenido distinto a tag
+    this._tagsHistory = this._tagsHistory.filter( oldTag => oldTag !== tag );
+
+    //* introduce en la primera posición de _tagsHistory el valor de tag
     this._tagsHistory.unshift( tag );
+
+    //* se queda solo con los 10 primeros valores de _tagsHistory
+    this._tagsHistory = this._tagsHistory.splice(0, 10);
+  }
+
+  searchTag( tag: string ): void {
+    //* remplaza los espacios múltiples por uno solo y elimina aquellos al principio y
+    //* al final del string
+    tag = tag.replace(/\s+/g, ' ').trim();
+    if ( tag.length === 0 ) return;
+    this.organizeHistory(tag);
 
     console.log(this._tagsHistory);
   }
