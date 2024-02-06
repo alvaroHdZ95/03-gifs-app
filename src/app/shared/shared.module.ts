4c805603db5a6512399214a5bf7a047f8e477efe
VN - Alvaro Hdz Valero <alvaro.hernandezv@viewnext.com>
2024-02-06 12:48:14 +0100

    S08 - Lazy Image - Parte 1

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
 
 
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    LazyImageComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    SidebarComponent
+    SidebarComponent,
+    LazyImageComponent //* Hay que exportarlo para usarlo fuera del módulo
   ]
 })
 export class SharedModule { }
